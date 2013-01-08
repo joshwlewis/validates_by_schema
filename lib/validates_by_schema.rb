d1f5e7c2b030358043006908aab36ce169cf92d2
Josh Lewis <josh.w.lewis@gmail.com>
2013-01-07 20:58:31 -0600

    Reduce complexity in validates_by_schema

--- lib/validates_by_schema.rb
+++ lib/validates_by_schema.rb
@@ -6,22 +6,25 @@ module ValidatesBySchema
   module ClassMethods
     def validates_by_schema options={}
       return unless table_exists?
-      # Don't auto validate primary keys or timestamps
-      columns = self.columns.reject(&:primary)
-      columns.reject!{|c| ['updated_at', 'created_at'].include? c.name}
+      columns = schema_validateable_columns
 
       # Allow user to specify :only or :except options
-      except = options.delete(:except).try(:map, &:to_s)
-      only = options.delete(:only).try(:map, &:to_s)
-      if only.present?
+      if only = options.delete(:only).try(:map, &:to_s)
         columns.select!{|c| only.include? c.name}
-      elsif except.present?
+      elsif except = options.delete(:except).try(:map, &:to_s)
         columns.reject!{|c| except.include? c.name}
       end
 
       columns.each do |c|
         vo = ValidationOption.new(c).to_hash
         validates c.name, vo if vo.present?
+      end
+    end
+
+    def schema_validateable_columns
+      # Don't auto validate primary keys or timestamps
+      columns.reject do |c| 
+        c.primary || %w(updated_at created_at).include?(c.name)
       end
     end
   end
