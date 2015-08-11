96de033854918985c3458ddaae8767e598b171af
Pascal Zumkehr <zumkehr@puzzle.ch>
2015-08-11 14:45:47 +0200

    test with default string limit

--- spec/config/schema.rb
+++ spec/config/schema.rb
@@ -14,21 +14,21 @@
 ActiveRecord::Schema.define(version: 20_121_210_034_140) do
   create_table 'widgets', force: true do |t|
     t.string 'name',          limit: 50
-    t.string 'model',         limit: 255,                              null: false
+    t.string 'model',                                                null: false
     t.text 'description'
-    t.integer 'wheels',                                                    null: false
+    t.integer 'wheels',                                              null: false
     t.integer 'doors',         limit: 2
     t.decimal 'price',                       precision: 6, scale: 2
     t.decimal 'cost',                        precision: 4, scale: 2, null: false
     t.decimal 'completion',                  precision: 3, scale: 3
     t.float 'rating'
-    t.float 'score',                                                     null: false
+    t.float 'score',                                                 null: false
     t.datetime 'published_at'
     t.date 'invented_on'
     t.time 'startup_time'
-    t.time 'shutdown_time',                                             null: false
+    t.time 'shutdown_time',                                          null: false
     t.boolean 'enabled'
     t.binary 'data'
-    t.integer 'parent_id', null: false
+    t.integer 'parent_id',                                           null: false
   end
 end
