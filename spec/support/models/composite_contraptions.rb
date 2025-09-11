4e27077af8a17ea1dd0feceb9a502e94da94e4f7
Joshua Buermann <buermann@gmail.com>
2025-09-11 13:05:30 -0700

    Remove CompositeContraption: composite primary keys should be validated like any other field in most circumstances, which is the default behavior.

--- spec/support/models/composite_contraptions.rb
+++
@@ -1,7 +0,0 @@
-require 'contraption'
-
-class CompositeContraption < Widget
-  self.primary_key = [:id, :legacy_id]
-  self.clear_validators!
-  validates_by_schema # recalculate the validations
-end
